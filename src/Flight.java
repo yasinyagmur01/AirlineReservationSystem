b3f7f615fbe46e9d1d246bdbeced1ec7b2c31d8b
Yasin Yağmur <yasin.yagmur88@gmail.com>
2026-02-04 23:02:51 +0300

    feat: add Flight model with SeatMap composition

---
+++ src/Flight.java
@@ -0,0 +1,43 @@
+import java.time.LocalDateTime;
+
+public class Flight {
+
+    private String flightNo;
+    private String origin;
+    private String destination;
+    private LocalDateTime deperatureTime;
+    private SeatMap seatMap;
+
+    public Flight(String flightNo,String origin,String destination,LocalDateTime deperatureTime,int rows,int cols){
+       this.flightNo=flightNo;
+       this.origin=origin;
+       this.destination=destination;
+       this.deperatureTime=deperatureTime;
+       this.seatMap= new SeatMap(rows,cols);
+    }
+
+    public String getFlightNo(){
+        return flightNo;
+    }
+
+    public String getOrigin(){
+        return origin;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public LocalDateTime getDeperatureTime(){
+        return deperatureTime;
+    }
+    public SeatMap getSeatMap(){
+        return seatMap;
+    }
+
+    @Override
+    public String toString(){
+        return flightNo+" | "+origin+" -> "+destination+" | "+deperatureTime;
+    }
+
+}
