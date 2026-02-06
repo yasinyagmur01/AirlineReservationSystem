0ad7c082f3e806de95685bf303ca49aa7a7c3322
Yasin Yağmur <yasin.yagmur88@gmail.com>
2026-02-06 05:13:58 +0300

    feat: add ReservationService with create, cancel and check-in logic

---
+++ src/RezervationService.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RezervationService {
+    //pnr-->reservations
+    private Map<String,Reservation> reservations;
+
+    public RezervationService(){
+        reservations = new HashMap<>();
+    }
+
+    public Reservation createReservations(Passenger p, Flight f, String seatLabel){
+        String pnr = "PNR"+reservations.size()+1;
+        Reservation reservation = new Reservation(pnr,p,f,seatLabel);
+        reservations.put(pnr,reservation);
+
+        return reservation;
+    }
+
+    public Reservation getReservation(String pnr){
+        return reservations.get(pnr.trim());
+    }
+
+    public boolean cancelReservation(String pnr){
+        Reservation r = getReservation(pnr);
+        if(r==null)return false;
+
+        r.cancel();
+        return true;
+    }
+
+    public boolean checkInPNR(String pnr){
+        Reservation r = getReservation(pnr);
+        if(r==null)return false;
+
+        r.checkIn();
+        return true;
+    }
+
+
+
+}
